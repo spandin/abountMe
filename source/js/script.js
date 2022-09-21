d4a4c6359068369aac7b5b8d47dca69c686650b7
spandin <willstesi@gmail.com>
2022-09-21 21:24:47 +0300

    script changes

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,67 @@
-const logoBlock = document.querySelector('.header-nav__logo')
+// 1
+window.addEventListener('load', (e) => {
+    console.log(e.type);
+})
 
-const subLogo = document.createElement('div')
-subLogo.innerHTML = 'frontend dev.'
-logoBlock.append(subLogo)
+// 2
+const image = document.querySelector('img')
+ 
+function imgReady() {
+    console.log(`Изображение: ${image.offsetWidth}x${image.offsetHeight}`);
+}
 
-subLogo.style.cssText = 'font-size: 20px; text-align: right;'
+document.addEventListener("DOMContentLoaded", imgReady);
+
+// 3
+window.addEventListener("load", () => {
+    console.log(`Изображение: ${image.offsetWidth}x${image.offsetHeight}`);
+});
+
+// Плавная прокрутка
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+//Довнлоад блокировщик
+(function () {
+    "use strict";
+
+    var el = document.createElement('div');
+    el.style.cssText = 'pointer-events:auto';
+
+    if (el.style.pointerEvents !== 'auto') {
+        el = null;
+
+        var _lock = function (evt) {
+            evt = evt || window.event;
+            var el = evt.target || evt.srcElement;
+            if (el && /\slocked\s/.test(' ' + el.className + ' ')) {
+                if (evt.stopPropagation) {
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                } else {
+                    evt.returnValue = true;
+                    evt.cancelBubble = true;
+                }
+            }
+        };
+
+        if (document.addEventListener) {
+            document.addEventListener('mousedown', _lock, false);
+            document.addEventListener('contextmenu', _lock, false);
+        } else {
+            document.attachEvent('onmousedown', _lock);
+            document.attachEvent('oncontextmenu', _lock);
+        }
+    }
+})();
