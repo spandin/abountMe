1b28cbad4c02b29cacfa039bfabb633ab506f40b
spandin <willstesi@gmail.com>
2022-09-28 00:18:59 +0300

    Menu list event

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', (e) => {
+    console.log('Page ' + e.type + 'ed');
 })
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
@@ -11,10 +12,18 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
 const burgerMenu = document.querySelector('.header-menu__burger')
 const menuList = document.querySelector('.header-menu__list')
 
-burgerMenu.addEventListener('click', (e) => {
+burgerMenu.addEventListener('click', () => {
     document.body.classList.toggle('_lock')
     burgerMenu.classList.toggle('_active')
     menuList.classList.toggle('_active')
+})
+
+menuList.addEventListener('click', (e) => {
+    if (e.target.closest('.header-nav__link')) {
+        document.body.classList.toggle('_lock')
+        burgerMenu.classList.toggle('_active')
+        menuList.classList.toggle('_active')
+    }
 })
 
 // Плавная прокрутка
