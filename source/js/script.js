ba7919620e260573ecc5b8e0666c7074d8a70363
spandin <willstesi@gmail.com>
2022-09-28 16:31:19 +0300

    small changes

--- source/js/script.js
+++ source/js/script.js
@@ -2,12 +2,6 @@ window.addEventListener('load', (e) => {
     console.log('Page ' + e.type + 'ed');
 })
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    document.body.classList.add('_touch')
-} else {
-    document.body.classList.add('_pc')
-}
-
 // Burger Nav
 const burgerMenu = document.querySelector('.header-menu__burger')
 const menuList = document.querySelector('.header-menu__list')
@@ -18,13 +12,26 @@ burgerMenu.addEventListener('click', () => {
     menuList.classList.toggle('_active')
 })
 
-menuList.addEventListener('click', (e) => {
-    if (e.target.closest('.header-nav__link')) {
-        document.body.classList.toggle('_lock')
-        burgerMenu.classList.toggle('_active')
-        menuList.classList.toggle('_active')
-    }
-})
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    document.body.classList.add('_touch')
+    menuList.addEventListener('click', (e) => {
+        if (e.target.closest('.header-nav__link')) {
+            document.body.classList.remove('_lock')
+            burgerMenu.classList.toggle('_active')
+            menuList.classList.toggle('_active')
+        }
+    })
+    
+} else {
+    document.body.classList.add('_pc')
+    menuList.addEventListener('click', (e) => {
+        if (e.target.closest('.header-nav__link')) {
+            document.body.classList.remove('_lock')
+            burgerMenu.classList.toggle('_active')
+            menuList.classList.toggle('_active')
+        }
+    })
+}
 
 // Плавная прокрутка
 const anchors = document.querySelectorAll('a[href*="#"]')
